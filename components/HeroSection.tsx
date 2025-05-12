08d17096cabf10cd874c71c499d0fb5419885b58
Maria Anh Thu Hoang <112003723+mariaathoang@users.noreply.github.com>
2025-05-12 16:06:42 +0200

    update: picture and title

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
-            src="/headshot.jpg"
+            src="/1000014276.jpg"
             alt="Headshot of Maria"
             width={325}
             height={325}
